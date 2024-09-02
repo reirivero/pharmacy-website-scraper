2543ab0412b2a69f5d1f462fc535feba1285e2a3
Reinaldo Rivero <reinaldo.rivero.ro@gmail.com>
2024-09-01 21:41:00 -0400

    Initial commit from desktop

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -1,15 +1,31 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
+# Import the libraries
+from datetime import timedelta, datetime
+# The DAG object; we'll need this to instatiate a DAG
+from airflow.models import DAG
+# Operators; you need this to write tasks!
+from airflow.operators.python import PythonOperator
 
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+# Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
+    'start_date': days_ago(0),# datetime(2023, 1, 1),
     'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
 }
 
-with DAG('example_dag', default_args=default_args, schedule_interval='@daily') as dag:
-    start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')
+# Define the DAG
+dag = DAG(
+    'example_dag',
+    default_args=default_args,
+    description='example dag',
+    schedule_interval=timedelta(days=1),
+)
 
-    start >> end
+# Define the tasks
+
+
+
+# Task pipeline
---
+++ src/extraction/extract_data.py
@@ -0,0 +1,18 @@
+import logging
+import time
+import json
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+if __name__ == '__main__':
+    pass
