9a568e42bda8702704cb2d5090414182a2efc9e8
Reinaldo Rivero <reinaldo.rivero.ro@gmail.com>
2024-09-04 00:23:59 -0400

    Updating repo: new files

---
+++ src/utils/pharmacy.py
@@ -0,0 +1,16 @@
+# Importar las posibles librerías
+import time
+import json
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
