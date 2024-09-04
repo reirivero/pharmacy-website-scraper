7c2faf0c195e6b1135095591d58f5788bd407d97
Reinaldo Rivero <reinaldo.rivero.ro@gmail.com>
2024-09-04 17:39:24 -0400

    extraction and loading data

--- src/extraction/extract_data.py
+++ src/extraction/extract_data.py
@@ -2,13 +2,111 @@ import logging
 import time
 import json
 import pandas as pd
+import os
+from datetime import datetime
+from src.utils import pharmacy as p
 
 # Leer el archivo .csv
-input_data = pd.read_csv('../../data/input_urls.csv')
+file_path = os.path.abspath('data/input_urls.csv')
+input_data = pd.read_csv(file_path)
 
-print(input_data.head(10))
-for row in input_data:
-    print(row, type(row),sep='\n')
+med_data = {}
 
-if __name__ == '__main__':
-    pass
+# Example of the dictionary 
+'''
+med_data = {
+    'Hormogel' : {
+        "Salcobrand" : {
+            "date" : datetime.now().strftime('%Y-%m-%d'),
+            "name": product_name,
+            "pharmacy" : pharmacy,
+            "web_name": name,
+            "is_available": is_available,
+            "price": price,
+            "bioequivalent": bioequivalent,
+            "active_principle": active_principle,
+            "sku": sku,
+            "lab_name": lab_name
+        },
+        "El Búho" : {
+            "date" : datetime.now().strftime('%Y-%m-%d'),
+            "name": product_name,
+            "pharmacy" : pharmacy,
+            "web_name": name,
+            "is_available": is_available,
+            "price": price,
+            "bioequivalent": bioequivalent,
+            "active_principle": active_principle,
+            "sku": sku,
+            "lab_name": lab_name
+        }      
+    },
+    'Rosuvastatina' : {
+        "Salcobrand : {
+            "date" : datetime.now().strftime('%Y-%m-%d'),
+            "name": product_name,
+            "pharmacy" : pharmacy,
+            "web_name": name,
+            "is_available": is_available,
+            "price": price,
+            "bioequivalent": bioequivalent,
+            "active_principle": active_principle,
+            "sku": sku,
+            "lab_name": lab_name
+        }
+    },
+}
+'''
+
+# Principle function to call a specific function depending on the pharmacy
+def extract_data(url,product_name,pharmacy) -> dict:
+    global data
+    data = {}
+
+    # Add info from input_urls.csv
+    data.update({
+        'date': datetime.now().strftime('%Y-%m-%d'),
+        'name': product_name,
+        'pharmacy': pharmacy
+    })
+
+    if 'buhochile.com' in url:
+        data = p.buhochile(url,data)
+    elif 'farmaciasahumada.cl' in url:
+        data = p.ahumada(url,data)
+    elif 'farmex.cl' in url:
+        data = p.farmex(url,data)
+    elif 'farmaciaelquimico.cl' in url:
+        data = p.elquimico(url,data)
+    elif 'salcobrand.cl' in url:
+        data = p.salcobrand(url,data)
+    elif 'novasalud.cl' in url:
+        data = p.novasalud(url,data)
+    elif 'drsimi.cl' in url:
+        data = p.drsimi(url,data)
+    elif 'ecofarmacias.cl' in url:
+        data = p.ecofarmacias(url,data)
+    elif 'mercadofarma.cl' in url:
+        data = p.mercadofarma(url,data)
+    else:
+        pass
+
+    # update the dict
+    if product_name not in med_data:
+        med_data[product_name] = {}
+    med_data[product_name][pharmacy] = data 
+
+
+# Iterar sobre cada fila del DataFrame
+for index, row in input_data.iterrows():
+    url = row['url']
+    product_name = row['product_name']
+    pharmacy = row['pharmacy']
+    
+    # Llamar a la función de extracción con los valores de la fila
+    extract_data(url, product_name, pharmacy)
+    # print(med_data)
+
+# print(med_data)
+# if __name__ == '__main__':
+#     pass
--- src/loading/load_data.py
+++ src/loading/load_data.py
@@ -1,5 +1,21 @@
 import logging
+import os
+from src.extraction.extract_data import med_data
+import pandas as pd
+
+file_path = os.path.abspath('data/output_urls.csv')
+
+output_data = []
 
 def load_data(data):
-    logging.info("Cargando datos en la base de datos")
+    # logging.info("Cargando datos en la base de datos")
     # Código para cargar los datos en la base de datos
+    for name, pharmacy in data.items():
+        for pharmacy, rowdata in pharmacy.items():
+            output_data.append(rowdata)
+
+    df = pd.DataFrame(output_data)
+    df.to_csv(file_path, index=False)
+
+if __name__ == '__main__':
+    load_data(med_data)
